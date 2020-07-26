59111da3a07631a0148c7b5e86de6edc01580d3d
Illya Sokolov <illya.sokolov82@gmail.com>
2020-07-26 17:04:55 +0300

    rendering all Tasks from TestData (without filtering by categories), removing html parts

--- todo/src/app/service/data-handler.service.ts
+++ todo/src/app/service/data-handler.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Category} from "../model/Category";
+import {Task} from "../model/Task";
 import {TestData} from "../data/TestData";
 
 @Injectable({
@@ -12,5 +13,9 @@ export class DataHandlerService {
 
   getCategories(): Category[] {
     return TestData.categories;
+  }
+
+  getTasks(): Task[] {
+    return TestData.tasks;
   }
 }
